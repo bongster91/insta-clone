2c6ee9a2af0bedce85dd7edbf1fb5ef8529f7082
Grace Bong <gracebong.park@gmail.com>
2023-02-16 17:08:14 -0500

    added basic user auth login functionality

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import authRoutes from './routes/auth.js';
 import { register } from './controllers/auth.js';
 
 /* CONFIGURATIONS */
@@ -51,6 +52,9 @@ const upload = multer({ storage })
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9999;
