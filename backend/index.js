8940d97c505ccdf35399465526db79f8f44aaf0f
Grace Bong <gracebong.park@gmail.com>
2023-02-16 16:21:57 -0500

    added mongdb config

--- backend/index.js
+++ backend/index.js
@@ -14,14 +14,27 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
+
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+
+app.use(helmet.crossOriginResourcePolicy({
+    policy: "cross-origin" 
+}));
 app.use(morgan('common'));
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(bodyParser.json({ 
+    limit: "30mb", extended: true 
+}));
+
+app.use(bodyParser.urlencoded({ 
+    limit: "30mb", extended: true 
+}));
+
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+app.use("/assets", express.static(
+    path.join(__dirname, 'public/assets')
+));
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
@@ -32,3 +45,17 @@ const storage = multer.diskStorage({
         cb(null, file.originalname);
     }
 });
+
+const upload = multer({ storage })
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9999;
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+}).then(() => {
+    app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`))
+
+}).catch((error) => console.log(`${error} did not connect`));
