a9e8039ee59bc6cce7e5751b8245c4d9a99f3702
Grace Bong <gracebong.park@gmail.com>
2023-02-19 23:56:48 -0500

    inserted posting function onto home page

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -11,7 +11,7 @@ const posts = express.Router({ mergeParams: true });
 
 // READ
 posts.get('/', verifyToken, getFeedPosts);
-posts.get('/:userId/posts', verifyToken, getUserPosts);
+posts.get('/:userId', verifyToken, getUserPosts);
 
 // UPDATE
 posts.patch('/:id/like', verifyToken, likePost);
